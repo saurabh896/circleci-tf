d1b63a5bf03eaaff7bb76ff397a918cf0ff01428
Saurabh Kadam <saurabhkdm721@gmail.com>
2023-12-13 14:53:24 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "google" {
   project     = "rakuten-analytics-rat"
   region      = "us-central1"
 }
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
